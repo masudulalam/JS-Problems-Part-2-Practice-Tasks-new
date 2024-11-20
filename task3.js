8e332d624cad6918f0bb5077d9e94e0d2087ace1
masudulalam <masuudulalam@gmail.com>
2024-11-21 03:43:27 +0600

    taskr.js is done

---
+++ task3.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 
+ * Task-3:
+Your task is to calculate the total budget required to buy electronics:
+
+    laptop = 35000 tk
+    tablet = 15000 tk
+    mobile = 20000 tk
+ */
+
+
+    
+// Solution:
+
