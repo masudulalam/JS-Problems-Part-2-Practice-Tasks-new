91dfa1c7be507d386a25799c212075abc0db2484
masudulalam <masuudulalam@gmail.com>
2024-11-21 17:07:01 +0600

    task3.js is solved

--- task3.js
+++ task3.js
@@ -2,14 +2,29 @@
  * 
  * 
  * Task-3:
-Your task is to calculate the total budget required to buy electronics:
+ * Your task is to calculate the total budget required to buy electronics:
 
     laptop = 35000 tk
     tablet = 15000 tk
     mobile = 20000 tk
+
+
+Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
+
  */
 
 
-    
-// Solution:
 
+// Solution:
+function calculateElectronicsBudget (numOfLaptop, numOfTablet, numOfMobile) {
+    const laptop = 35000;
+    const tablet = 15000;
+    const mobile = 20000;
+
+    const totalMoney = laptop * numOfLaptop + tablet * numOfTablet + mobile * numOfMobile;
+
+    return totalMoney;
+}
+
+const output= calculateElectronicsBudget(3, 1, 3);
+console.log(output);
